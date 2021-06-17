6f3a62dc5e78e1296f3bbac43a018438b872721e
PuzzyCooler <reinis.net123@gmail.com>
2021-06-17 13:31:50 +0300

    Pirmais jautājums.

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -3,4 +3,17 @@ using namespace std;
 int main(){
 
 int a1, a2, a3, src=0;
+do{
+cout << "Tests";
+cout<<"\n*******Kurs no siem atbilzu variantiem nav simbolu virkne?*******";
+cout<<"\n1. String";
+cout<<"\n2. boolean";
+cout<<"\n3. char";
+cout<<"\n4. int\n";
+cin>>a1;
+if(a1==4){
+	src++;
+}
+}
+while(a1!=4);
 }
